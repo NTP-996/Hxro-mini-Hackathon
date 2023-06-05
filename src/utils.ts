6fa6d5e021d71979c62bfcac0f029fd11e573301
Peter <31298991+NTP-996@users.noreply.github.com>
2023-06-06 06:55:52 +0700

    Update utils.ts

--- src/utils.ts
+++ src/utils.ts
@@ -5,18 +5,13 @@ const rpc =
   "https://rpc-devnet.helius.xyz/?api-key=7aa8cc25-5a61-4548-99d4-e2e3f3aeb2bf";
 const connection = new web3.Connection(rpc, "confirmed");
 const keypair = web3.Keypair.fromSecretKey(
-  new Uint8Array([
-    101, 175, 10, 186, 49, 139, 133, 31, 61, 29, 166, 88, 225, 71, 210, 115,
-    151, 117, 24, 76, 71, 161, 157, 163, 63, 158, 210, 89, 155, 100, 226, 96,
-    63, 72, 101, 56, 32, 244, 183, 107, 65, 97, 199, 133, 94, 221, 152, 36, 170,
-    83, 110, 117, 70, 184, 133, 165, 164, 17, 138, 247, 130, 85, 230, 52,
-  ])
+  new Uint8Array(PRIVATE_KEY)
 );
 
 const config = psdk.DEV_CONFIG;
 
 const parimutuelWeb3 = new psdk.ParimutuelWeb3(config, connection);
-const marketPair = psdk.MarketPairEnum.BTCUSD;
+const marketPair = psdk.MarketPairEnum.ETHUSD;
 const duration = 300; // 5min
 const markets = psdk.getMarketPubkeys(config, marketPair);
 const marketFiltered = markets.filter((m) => m.duration === duration);
